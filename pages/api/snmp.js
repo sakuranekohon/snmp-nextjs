58bb09a5286e8cf61190298d9f9d4c4f7d1b2b21
sakuranekohon <114348771+sakuranekohon@users.noreply.github.com>
2023-11-01 03:08:29 +0800

    finish

--- pages/api/snmp.js
+++ pages/api/snmp.js
@@ -2,21 +2,33 @@ const snmp = require("net-snmp");
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
-        const { targetIP, length ,OIDs } = req.body;
-        console.log("snmp.js",OIDs)
+        const { targetIP, length, OIDs } = req.body;
+        console.log("snmp.js", OIDs)
         const session = snmp.createSession(targetIP, "public"); //建立與設備的通訊
 
-        session.get(OIDs, (error, varbinds) => {   //varbinds為OID所對應的資訊 如:sysName : ES-2108
-            if (error) {
-                console.error(error);
+        // session.get(OIDs, (error, varbinds) => {   //varbinds為OID所對應的資訊 如:sysName : ES-2108
+        //     if (error) {
+        //         console.error(error);
+        //         res.status(500).end("SNMP request failed");
+        //     } else {
+        //         console.log("Result:", varbinds[0].value.toString());
+        //         res.status(200).json({
+        //             result: varbinds[0].value.toString()
+        //         });
+        //     }
+        //     session.close();    //結束通訊
+        // });
+        session.get(OIDs,(error,varbinds)=>{
+            if(error){
+                console.error(error)
                 res.status(500).end("SNMP request failed");
-            } else {
-                console.log("Result:", varbinds[0].value.toString());
+            }else{
+                console.log("Result:", varbinds);
                 res.status(200).json({
-                    result: varbinds[0].value.toString()
+                    result:Array.from(varbinds).map((element)=>element.value.toString())
                 });
             }
-            session.close();    //結束通訊
+            session.close();
         });
     } else {
         res.status(405).end();
